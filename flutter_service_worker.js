93e2f3f5dcb3a8679961ac3c73b9dc85cc7524a2
win.zou <win.zou@razer.com>
2020-03-20 09:46:07 +0800

    1、网址  https://winajoin.github.io/pmp_flutter_web/

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -11,7 +11,7 @@ const RESOURCES = {
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "8cecd862ae773d8f3fc06664356bb359",
-"main.dart.js": "1ce5ac8e260ac9ab6971d3186c5dd64c",
+"main.dart.js": "247e70155133e85100a256cb22000c15",
 "manifest.json": "e4be876827f8f9c7d4ae95b9c2bb9928"
 };
 
